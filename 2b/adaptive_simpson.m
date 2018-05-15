c12d1c768bce4ab02b0deed67240c9e6ea59eee5
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2018-05-15 10:46:35 +0700

    adaptive simpson implementation

---
+++ 2b/adaptive_simpson.m
@@ -0,0 +1,16 @@
+function res = adaptive_simpson(f, a, b, tol)
+  total = simpson_rule(f,a,b);
+  res = adaptive_simpson_helper(f, a, b, tol, total);
+endfunction
+
+function res = adaptive_simpson_helper(f, a, b, tol, total)
+  c = (a+b)/2;
+  left = simpson_rule(f, a, c);
+  right = simpson_rule(f, c, b);
+  
+  if abs(left + right - total) <= 15 * tol
+    res = left + right + (left + right - total)/15;
+  else
+    res = adaptive_simpson_helper(f, a, c, tol/2, left) + adaptive_simpson_helper(f, c, b, tol/2, right);
+  endif
+endfunction
---
+++ 2b/simpson_rule.m
@@ -0,0 +1,3 @@
+function res = simpson_rule(f, a, b)
+  res = ((b-a)/6) * (f(a) + 4*f((a+b)/2) + f(b));
+end
