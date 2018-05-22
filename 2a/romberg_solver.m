f7f03c38776baeada16aa6c3e71204421a0e8934
Satria Bagus <satria_bagus@hotmail.com>
2018-05-22 11:44:24 +0700

    romberg

--- 2a/romberg_solver.m
+++ 2a/romberg_solver.m
@@ -1,1 +1,1 @@
-format long;n = [8, 64, 128];f = @(x) e^x;g = @(x) cos(x);for i = 1:length(eps)  tic;  res = romberg(f, 0, 1 , n(i)) * romberg(g, 0, pi/2, n(1))  toc;endfor
+format long;n = [8, 64];f = @(x) e^x;g = @(x) cos(x);for i = 1:length(n)  tic;  [res_f, err_f] = romberg(f, 0, 1 , n(i));  [res_g, err_g] = romberg(g, 0, pi/2, n(i));  res = res_f * res_g;  err = err_f * err_g;  time = toc;  disp(i)  disp(time)  disp(res)  disp(err)endfor
