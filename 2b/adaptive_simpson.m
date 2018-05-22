02f22868370a2a28fe1a1f401a1f56d67fa8f6e5
Fersandi Pratama <fersandi_pratama@rocketmail.com>
2018-05-22 16:19:29 +0700

    change efficiency

--- 2b/adaptive_simpson.m
+++ 2b/adaptive_simpson.m
@@ -1,7 +1,7 @@
 function res = adaptive_simpson(f, a, b, tol)
   total = simpson_rule(f,a,b);
   res = adaptive_simpson_helper(f, a, b, tol, total);
-  res(2) += 1;
+  res(2) += 3;
 endfunction
 
 function res = adaptive_simpson_helper(f, a, b, tol, total)
@@ -10,7 +10,7 @@ function res = adaptive_simpson_helper(f, a, b, tol, total)
   right = simpson_rule(f, c, b);
   
   if abs(left + right - total) <= 15 * tol
-    res = [left + right + (left + right - total)/15, 2];
+    res = [left + right + (left + right - total)/15, 6];
   else
     res = adaptive_simpson_helper(f, a, c, tol/2, left) + adaptive_simpson_helper(f, c, b, tol/2, right);
   endif
